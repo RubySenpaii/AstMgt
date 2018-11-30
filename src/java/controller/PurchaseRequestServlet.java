bda043f2d17383ef9f52a27228862ec7f9bd0d6a
Bryll Joey Delfin <Bryll Joey Delfin@MSI>
2018-12-01 04:35:24 +0800

    Finished updating rejection for remarks column update

--- src/java/controller/PurchaseRequestServlet.java
+++ src/java/controller/PurchaseRequestServlet.java
@@ -80,8 +80,9 @@ public class PurchaseRequestServlet extends BaseServlet {
         int prid = Integer.parseInt(request.getParameter("prid"));
         HttpSession session = request.getSession();
         Employee employee = (Employee) session.getAttribute("user");
+        String remarks = request.getParameter("remarks");
         try {
-            prservice.RejectPurchaseRequest(employee.EmployeeId, prid);
+            prservice.RejectPurchaseRequest(employee.EmployeeId, prid, remarks);
             return "/PurchaseRequest/List";
         } catch (Exception e) {
             System.out.println("Error in PR servlet" + e);
